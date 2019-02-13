88945ce31edfbad221538ba27a3a9f5c0467db63
Dexter Hennington <Batman@dexters-mbp.attlocal.net>
2019-02-13 18:00:40 -0500

    Views

--- photo-feed/App.js
+++ photo-feed/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation'
+
+import feed from './app/screens/feed'
+import upload from './app/screens/upload'
+import profile from './app/screens/profile'
+
+const Mainstack = createBottomTabNavigator (
+  {
+    Feed:{screen:feed},
+    Upload:{screen:upload},
+    Profile:{screen:profile}
+  }
+)
 
 export default class App extends React.Component {
   render() {
